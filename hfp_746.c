914488a2d4a95f8ed385c9711bf39f89baff8ad5
Vlad Balmos <vlad.balmos@gmail.com>
2023-06-17 14:55:43 +0300

    WIP

--- hfp_746.c
+++ hfp_746.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
@@ -8,21 +9,20 @@
 #include "debug.h"
 
 #define MICRO_SLEEP_MS 1000
-// 48mhz / 16khz - 1 = 2999 - audio range of 0 - 8000hz
-#define ADC_SAMPLING_RATE 48 * 1000 * 1000 / 16000 - 1
-// #define CONV_FACTOR_12b 3.3f / (1 << 12)
-// #define CONV_FACTOR_10b 2 * 2.52f / (1 << 10)
+#define BIT_RATE 10
 
-#define MAX_SAMPLES 8
+#define MAX_SAMPLES 255
+#define MAX_SINE_VALUE ((1 << 10) / 2) - 1
+#define PI 3.14159265
+#define SAMPLE_RATE 8000
 
 #define SPI_PORT spi0
 #define SCLK 2
 #define MOSI 3
 #define CS 5
 
-uint16_t samples_12b[MAX_SAMPLES];
-
-int dma_chan;
+int16_t sine_wave[MAX_SAMPLES];
+uint16_t biased_sine_wave[MAX_SAMPLES];
 
 void print_binary(uint16_t n) {
     for (int i = 15; i >= 0; i--) {
@@ -33,9 +33,32 @@ void print_binary(uint16_t n) {
     printf("\n");
 }
 
-void dma_handler() {
-    dma_hw->ints0 = 1u << dma_chan;
-    dma_channel_set_write_addr(dma_chan, samples_12b, false);
+double voltage(int16_t sample, uint16_t bitrate, float ref_voltage) {
+    double conv_factor = ref_voltage / (1 << BIT_RATE);
+    
+    double voltage = sample * conv_factor;
+    return voltage;
+}
+
+double voltage_3_3(int16_t sample, uint16_t bitrate) {
+    return voltage(sample, bitrate, 3.3f);
+}
+
+double voltage_5(int16_t sample, uint16_t bitrate) {
+    return voltage(sample, bitrate, 5.0f);
+}
+
+// Generate sine wave at frequency
+uint16_t generate_sine_wave(uint16_t frequency, int16_t *buffer) {
+    const uint16_t samples = SAMPLE_RATE / frequency;
+    
+    for (uint16_t i = 0; i < samples; i++) {
+        double angle = (double)i / samples * 2.0 * PI;
+        double sine_val = sin(angle);
+        buffer[i] = round(sine_val * MAX_SINE_VALUE);
+    }
+    
+    return samples;
 }
 
 int main() {
@@ -59,65 +82,44 @@ int main() {
     gpio_set_dir(CS, GPIO_OUT);
     gpio_put(CS, 1);
     
-
-    // Make sure GPIO is high-impedance, no pullups etc
-    adc_init();
-
-    adc_gpio_init(26);
-    // Select ADC input 0 (GPIO26)
-    adc_select_input(0);
-    adc_fifo_setup(
-        true,
-        true,
-        1,
-        false,
-        false
-    );
-    adc_set_clkdiv(ADC_SAMPLING_RATE);
+    uint16_t freq = 440;
     
-    dma_chan = dma_claim_unused_channel(true);
-    dma_channel_config cfg = dma_channel_get_default_config(dma_chan);
-    channel_config_set_transfer_data_size(&cfg, DMA_SIZE_16);
-    channel_config_set_read_increment(&cfg, false);
-    channel_config_set_write_increment(&cfg, true);
-    channel_config_set_dreq(&cfg, DREQ_ADC);
-
-    dma_channel_configure(dma_chan, &cfg,
-        samples_12b,
-        &adc_hw->fifo,
-        MAX_SAMPLES,
-        true
-    );
-        
-    dma_channel_set_irq0_enabled(dma_chan, true);
-    irq_set_exclusive_handler(DMA_IRQ_0, dma_handler);
-    irq_set_enabled(DMA_IRQ_0, true);
-        
-    printf("Capturing\n");
-    adc_run(true);
+    uint16_t samples_num = generate_sine_wave(freq, sine_wave);
+    printf("Generated %d samples for %dhz@%dkhz\n", samples_num, freq, SAMPLE_RATE / 1000);
+    
+    for (uint16_t i = 0; i < samples_num; i++) {
+        int16_t sample = sine_wave[i];
+        biased_sine_wave[i] = sample + MAX_SINE_VALUE;
+        printf("%d %d |", sample, biased_sine_wave[i]);
+    }
+    
+    printf("\n");
+ 
+    for (uint16_t i = 0; i < samples_num; i++) {
+        int16_t sample = sine_wave[i];
+        printf("%f %f |", voltage_5(sample, BIT_RATE), voltage_5(biased_sine_wave[i], BIT_RATE));
+    }
+    printf("\n");
     
     while (true) {
-        __wfi();
         absolute_time_t last_sent = nil_time;
         absolute_time_t now;
         
         uint i = 0;
         
-        while (i < MAX_SAMPLES) {
+        while (i < samples_num) {
             now = get_absolute_time();
             uint32_t diff = absolute_time_diff_us(last_sent, now);
-            if (diff >= 62) {
-                uint16_t sample_12b[1] = {samples_12b[i]};
+            if (diff >= 125) {
+                uint16_t spi_samples[1] = {biased_sine_wave[i] << 2}; // 10 bit conversion
                 
                 gpio_put(CS, 0);
-                spi_write16_blocking(SPI_PORT, sample_12b, 1);
+                spi_write16_blocking(SPI_PORT, spi_samples, 1);
                 gpio_put(CS, 1);
                 i++;
                 last_sent = now;
             }
             sleep_us(5);
         }
-            
-        dma_channel_start(dma_chan);
     }
 }
