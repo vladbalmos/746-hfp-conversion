8482baede1e90d1f0af3f9db4d3a1344f197b77e
Vlad Balmos <vlad.balmos@gmail.com>
2023-07-31 22:25:52 +0300

    WIP

--- main/bt.c
+++ main/bt.c
@@ -55,8 +55,8 @@ const static char *c_hf_evt_str[] = {
     "RING_IND_EVT",                      /*!< ring indication event */
 };
 
-
-const static char *c_connection_state_str[] = {
+// esp_hf_client_connection_state_t
+const char *c_connection_state_str[] = {
     "disconnected",
     "connecting",
     "connected",
@@ -64,11 +64,112 @@ const static char *c_connection_state_str[] = {
     "disconnecting",
 };
 
-const static char *c_audio_state_str[] = {
+// esp_hf_client_audio_state_t
+const char *c_audio_state_str[] = {
     "disconnected",
     "connecting",
     "connected",
     "connected_msbc",
+};
+
+/// esp_hf_vr_state_t
+const char *c_vr_state_str[] = {
+    "disabled",
+    "enabled",
+};
+
+// esp_hf_service_availability_status_t
+const char *c_service_availability_status_str[] = {
+    "unavailable",
+    "available",
+};
+
+// esp_hf_roaming_status_t
+const char *c_roaming_status_str[] = {
+    "inactive",
+    "active",
+};
+
+// esp_hf_client_call_state_t
+const char *c_call_str[] = {
+    "NO call in progress",
+    "call in progress",
+};
+
+// esp_hf_client_callsetup_t
+const char *c_call_setup_str[] = {
+    "NONE",
+    "INCOMING",
+    "OUTGOING_DIALING",
+    "OUTGOING_ALERTING"
+};
+
+// esp_hf_client_callheld_t
+const char *c_call_held_str[] = {
+    "NONE held",
+    "Held and Active",
+    "Held",
+};
+
+// esp_hf_response_and_hold_status_t
+const char *c_resp_and_hold_str[] = {
+    "HELD",
+    "HELD ACCEPTED",
+    "HELD REJECTED",
+};
+
+// esp_hf_client_call_direction_t
+const char *c_call_dir_str[] = {
+    "outgoing",
+    "incoming",
+};
+
+// esp_hf_client_call_state_t
+const char *c_call_state_str[] = {
+    "active",
+    "held",
+    "dialing",
+    "alerting",
+    "incoming",
+    "waiting",
+    "held_by_resp_hold",
+};
+
+// esp_hf_current_call_mpty_type_t
+const char *c_call_mpty_type_str[] = {
+    "single",
+    "multi",
+};
+
+// esp_hf_volume_control_target_t
+const char *c_volume_control_target_str[] = {
+    "SPEAKER",
+    "MICROPHONE"
+};
+
+// esp_hf_at_response_code_t
+const char *c_at_response_code_str[] = {
+    "OK",
+    "ERROR"
+    "ERR_NO_CARRIER",
+    "ERR_BUSY",
+    "ERR_NO_ANSWER",
+    "ERR_DELAYED",
+    "ERR_BLACKLILSTED",
+    "ERR_CME",
+};
+
+// esp_hf_subscriber_service_type_t
+const char *c_subscriber_service_type_str[] = {
+    "unknown",
+    "voice",
+    "fax",
+};
+
+// esp_hf_client_in_band_ring_state_t
+const char *c_inband_ring_state_str[] = {
+    "NOT provided",
+    "Provided",
 };
 
 // static TaskHandle_t bt_audio_out_task = NULL;
@@ -116,7 +217,7 @@ static void bt_hf_incoming_data_callback(const uint8_t *buf, uint32_t size) {
         dac_audio_buffer_t *audio_buf = dac_audio_take_free_buffer_safe(audio_out_buf_pool, portMAX_DELAY);
         if (audio_buf == NULL) {
             // @TODO: add warning statistics
-            // ESP_LOGE(BT_TAG, "No free buffer available");
+            ESP_LOGE(BT_TAG, "No free buffer available");
             return;
         }
         
@@ -260,10 +361,142 @@ static void esp_bt_hf_client_callback(esp_hf_client_cb_event_t event, esp_hf_cli
             
             break;
         }
-                                            
-        default: {
-            ESP_LOGD(BT_TAG, "Unhandled HF event: %d", event);
+
+        case ESP_HF_CLIENT_BVRA_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--VR state %s",
+                    c_vr_state_str[param->bvra.value]);
+            break;
         }
+
+        case ESP_HF_CLIENT_CIND_SERVICE_AVAILABILITY_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--NETWORK STATE %s",
+                    c_service_availability_status_str[param->service_availability.status]);
+            break;
+        }
+
+        case ESP_HF_CLIENT_CIND_ROAMING_STATUS_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--ROAMING: %s",
+                    c_roaming_status_str[param->roaming.status]);
+            break;
+        }
+
+        case ESP_HF_CLIENT_CIND_SIGNAL_STRENGTH_EVT:
+        {
+            ESP_LOGI(BT_TAG, "-- signal strength: %d",
+                    param->signal_strength.value);
+            break;
+        }
+
+        case ESP_HF_CLIENT_CIND_BATTERY_LEVEL_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--battery level %d",
+                    param->battery_level.value);
+            break;
+        }
+
+        case ESP_HF_CLIENT_COPS_CURRENT_OPERATOR_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--operator name: %s",
+                    param->cops.name);
+            break;
+        }
+
+        case ESP_HF_CLIENT_CIND_CALL_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--Call indicator %s",
+                    c_call_str[param->call.status]);
+            break;
+        }
+
+        case ESP_HF_CLIENT_CIND_CALL_SETUP_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--Call setup indicator %s",
+                    c_call_setup_str[param->call_setup.status]);
+            break;
+        }
+
+        case ESP_HF_CLIENT_CIND_CALL_HELD_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--Call held indicator %s",
+                    c_call_held_str[param->call_held.status]);
+            break;
+        }
+
+        case ESP_HF_CLIENT_BTRH_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--response and hold %s",
+                    c_resp_and_hold_str[param->btrh.status]);
+            break;
+        }
+
+        case ESP_HF_CLIENT_CLIP_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--clip number %s",
+                    (param->clip.number == NULL) ? "NULL" : (param->clip.number));
+            break;
+        }
+
+        case ESP_HF_CLIENT_CCWA_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--call_waiting %s",
+                    (param->ccwa.number == NULL) ? "NULL" : (param->ccwa.number));
+            break;
+        }
+
+        case ESP_HF_CLIENT_CLCC_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--Current call: idx %d, dir %s, state %s, mpty %s, number %s",
+                    param->clcc.idx,
+                    c_call_dir_str[param->clcc.dir],
+                    c_call_state_str[param->clcc.status],
+                    c_call_mpty_type_str[param->clcc.mpty],
+                    (param->clcc.number == NULL) ? "NULL" : (param->clcc.number));
+            break;
+        }
+
+        case ESP_HF_CLIENT_VOLUME_CONTROL_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--volume_target: %s, volume %d",
+                    c_volume_control_target_str[param->volume_control.type],
+                    param->volume_control.volume);
+            break;
+        }
+
+        case ESP_HF_CLIENT_AT_RESPONSE_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--AT response event, code %d, cme %d",
+                    param->at_response.code, param->at_response.cme);
+            break;
+        }
+
+        case ESP_HF_CLIENT_CNUM_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--subscriber type %s, number %s",
+                    c_subscriber_service_type_str[param->cnum.type],
+                    (param->cnum.number == NULL) ? "NULL" : param->cnum.number);
+            break;
+        }
+
+        case ESP_HF_CLIENT_BSIR_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--inband ring state %s",
+                    c_inband_ring_state_str[param->bsir.state]);
+            break;
+        }
+
+        case ESP_HF_CLIENT_BINP_EVT:
+        {
+            ESP_LOGI(BT_TAG, "--last voice tag number: %s",
+                    (param->binp.number == NULL) ? "NULL" : param->binp.number);
+            break;
+        }
+
+        default:
+            ESP_LOGE(BT_TAG, "HF_CLIENT EVT: %d", event);
+            break;
 
         
     }
